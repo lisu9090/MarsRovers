347183d9dd9242b43d8a0fbb5b42273a14d71225
tomasz.lis <tomasz.lis@comarch.pl>
2020-01-28 11:02:56 +0100

    Apps architecture designed end created.

---
+++ MarsRovers/Controllers/Abstract/IRoverController.cs
@@ -0,0 +1,15 @@
+﻿using MarsRoversInfrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRovers.Controllers.Abstract
+{
+    interface IRoversController
+    {
+        int CreatePlateau(int x, int y);
+        int CreateRover(int x, int y, string direction);
+        int SetMovementInstructons(string instructions);
+        IEnumerable<BaseModel> GetRoversPositions();
+    }
+}
---
+++ MarsRoversInfrastructure/Views/IView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoversInfrastructure.Views
+{
+    interface IView
+    {
+        string Process(string input);
+    }
+}
