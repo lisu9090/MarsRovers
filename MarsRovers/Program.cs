d12937e84d6f7f4d1a265aa11a4d27a73ab65366
tomasz.lis <tomasz.lis@comarch.pl>
2020-01-28 09:38:19 +0100

    Initial commit

---
+++ MarsRovers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRovers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
