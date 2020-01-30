d8f981ccc6b44ede8d016dde7da1e501622efe25
lisu9090 <lisu9090@gmail.com>
2020-01-30 14:03:54 +0100

    Tests compleated

---
+++ MarsRoversTest/Repositories/PlateauRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using MarsRovers.Models;
+using MarsRovers.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarsRovers.UnitTests.Repositories
+{
+    public class PlateauRepositoryTest
+    {
+        [Fact]
+        public void AddGetModel_ReceivesModel_ReturnsModel()
+        {
+            var plateauRepository = new PlateauRepository();
+            var expectedData = new PlateauModel(1, 2);
+
+            plateauRepository.AddModel(expectedData);
+            var dataEnummerator = plateauRepository.GetModels().GetEnumerator();
+            dataEnummerator.MoveNext();
+            var actualData = dataEnummerator.Current;
+
+            Assert.Equal(expectedData, (PlateauModel)actualData);
+        }
+
+        [Fact]
+        public void Reset_ClearsRepository_ReturnsEmptyEnummerator()
+        {
+            var plateauRepository = new PlateauRepository();
+            var sampleData = new PlateauModel(1, 2);
+            int expectedData = 0;
+            var actualData = 0;
+
+            plateauRepository.AddModel(sampleData);
+            plateauRepository.Reset();
+
+            var dataEnummerator = plateauRepository.GetModels().GetEnumerator();
+            while (dataEnummerator.MoveNext())
+            {
+                actualData++;
+            }
+
+            Assert.Equal(expectedData, actualData);
+        }
+    }
+}
---
+++ MarsRoversTest/Repositories/RoversRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using MarsRovers.Models;
+using MarsRovers.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarsRovers.UnitTests.Repositories
+{
+    public class RoversRepositoryTest
+    {
+        [Fact]
+        public void AddGetModel_ReceivesModel_ReturnsModel()
+        {
+            var roversRepository = new RoversRepository();
+            var expectedData = new RoverModel(1, 2, "E");
+
+            roversRepository.AddModel(expectedData);
+            var dataEnummerator = roversRepository.GetModels().GetEnumerator();
+            dataEnummerator.MoveNext();
+            var actualData = dataEnummerator.Current;
+
+            Assert.Equal(expectedData, (RoverModel)actualData);
+        }
+
+        [Fact]
+        public void Reset_ClearsRepository_ReturnsEmptyEnummerator()
+        {
+            var roversRepository = new RoversRepository();
+            var sampleData = new RoverModel(1, 2, "E");
+            int expectedData = 0;
+            var actualData = 0;
+
+            roversRepository.AddModel(sampleData);
+            roversRepository.Reset();
+
+            var dataEnummerator = roversRepository.GetModels().GetEnumerator();
+            while (dataEnummerator.MoveNext())
+            {
+                actualData++;
+            }
+
+            Assert.Equal(expectedData, actualData);
+        }
+
+        [Fact]
+        public void UpdateRoverMovementInstructions_ReceivesInstructions_UpdatesLastModel()
+        {
+            var roversRepository = new RoversRepository();
+            var expectedData = "RMMRMMRRMR";
+
+            roversRepository.AddModel(new RoverModel(1, 2, "E"));
+            roversRepository.UpdateRoverMovementInstructions(expectedData);
+
+            var dataEnummerator = roversRepository.GetModels().GetEnumerator();
+            dataEnummerator.MoveNext();
+            var actualData = ((RoverModel)dataEnummerator.Current).MovementInstructions;
+
+            Assert.Equal(expectedData, actualData);
+        }
+    }
+}
